d5ff3dd3a92bf6007ed4f9451432403b471ffc8a
Mustufa <memonmustufa890@gmail.com>
2026-07-10 16:52:12 +0500

    add vercel.json for spa routing

--- src/page/task.jsx
+++ src/page/task.jsx
@@ -62,7 +62,7 @@ const Task = () => {
   value={name} onChange={(e)=> setName(e.target.value)}
   />
   <button className='flex-1 bg-purple-900 text-white font-bold p-3 rounded-xl hover:bg-purple-700' onClick={addData} >Add Task</button>     
-   <input className='w-full rounded-xl border border-gray-400 p-3' type="text" placeholder='Enter your Task' 
+   <input className='w-full rounded-xl border border-gray-400 p-3' type="text" placeholder='Enter your Number' 
   value={phone} onChange={(e)=> setTask(e.target.value)}
   />
      
